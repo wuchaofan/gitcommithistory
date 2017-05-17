855045b15a48a53f4387dcc89dc9f4aebba1c439
chaofan <915546302@163.com>
2017-05-17 23:14:46 +0800

    modified:   README.md 	modified:   gitlog.js

--- gitlog.js
+++ gitlog.js
@@ -21,17 +21,26 @@ var optionDef = [
         name: 'help',
         alias: 'h',
         type: Boolean
+    },{
+        name: 'dir',
+        alias: 'd',
+        type: String
     }
 ]
 
 function gitlog(){
     var option = commandLineArgs(optionDef)
+
     if (option.help){
         console.log('参数：\n'+
     '--begin, -b 表示开始时间，例子："2017-05-16"、"2017-05-16 11:12"、"2017-05-16T10:00:37.855Z" \n'+
     '--end, -e 表示结束时间\n'+
-    '--search, -s 搜索内容')
+    '--search, -s 搜索内容\n' +
+    '--dir, -d 指定git库路径，默认为当前目录')
         return
+    }
+    if (option.dir){
+        process.chdir(option.dir);
     }
     var commandline = "git log -n 200 "
     if (option.begin){
